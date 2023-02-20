74f0c2fc348715bd68719916992bc9018632e68d
유성민 <growth_s@naver.com>
2023-02-20 18:35:00 +0900

    ImageSave 변경 전

---
+++ auth3/src/main/java/com/example/auth3/configuration/S3Config.java
@@ -0,0 +1,34 @@
+package com.example.auth3.configuration;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+    @Value("cloud.aws.credentials.accessKey")
+    private String accessKey;
+    @Value("cloud.aws.credentials.secretKey")
+    private String secretKey;
+    @Value("cloud.aws.s3.bucketName")
+    private String bucketName;
+    @Value("cloud.aws.region.static")
+    private String region;
+
+    @Bean
+    public AmazonS3 s3Builder() {
+        AWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        PutObjectRequest()
+
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+                .withRegion(region).build();
+    }
+}
